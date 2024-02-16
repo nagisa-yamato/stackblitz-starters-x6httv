8cb59037aa0613e6d9a79b35ed694d97fdd73ce5
Shogo Yamato <yamato@nagisa-inc.jp>
2024-02-16 15:06:43 +0900

    build(npm): added typescript as devDependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import { FC } from 'react';
-import './style.css';
+import { FC } from "react";
+import "./style.css";
 // Module '"@react-hookz/web"' has no exported member 'useCookieValue'.(2305)
-import { useCookieValue } from '@react-hookz/web';
+// import { useCookieValue } from '@react-hookz/web';
 // Cannot find module '@react-hookz/web/esm/useCookieValue' or its corresponding type declarations.(2307)
 // import { useCookieValue } from '@react-hookz/web/esm/useCookieValue';
 // Cannot find module '@react-hookz/web/useCookieValue/index.js' or its corresponding type declarations.(2307)
-// import { useCookieValue } from '@react-hookz/web/useCookieValue/index.js';
+import { useCookieValue } from "@react-hookz/web/useCookieValue/index.js";
 
 export const App: FC<{ name: string }> = ({ name }) => {
-  const [cookie] = useCookieValue('foo');
+  const [cookie] = useCookieValue("foo");
   console.log(cookie);
 
   return (
